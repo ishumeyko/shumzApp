3d0982ebc2efa6ad6e5eb510bbacbc175b1c9fd7
Igor Shumeyko <ishumeyko@cogniance.com>
2013-11-14 23:28:37 +0200

    0003. Splash activity rotation bugs are fixed.

--- Application/src/com/shumz/application/SplashActivity.java
+++ Application/src/com/shumz/application/SplashActivity.java
@@ -1,7 +1,5 @@
 package com.shumz.application;
 
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -14,62 +12,78 @@ import android.widget.RelativeLayout;
 
 public class SplashActivity extends Activity implements OnTouchListener {
 
-	// Called when the activity is first created
-	MediaPlayer logoMusic;
-	boolean isTouched = false;
+	private static RelativeLayout splashLayout;
+
+	private static MediaPlayer logoMusic;
+	private static boolean isTouched = false;
+	private static boolean isRotated = false;
+
+	private static final String IS_ROTATED = "isRotated";
+
+	Thread logoTimer = new Thread() {
+		public void run() {
+			try {
+
+				for (int i = 0; i < 78; i++) {
+					if (isTouched) {
+						break;
+					} else {
+						sleep(100);
+					}
+				}
+
+				Intent menuIntent = new Intent(
+						"com.shumz.application.MAINACTIVITY");
+				startActivity(menuIntent);
+			}
+
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			finally {
+				logoMusic.stop();
+				logoMusic.release();
+				finish();
+			}
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle ShumIsCool) {
 		super.onCreate(ShumIsCool);
 		setContentView(R.layout.activity_splash);
 
-		RelativeLayout splashLayout = (RelativeLayout) findViewById(R.id.splash_layout);
+		splashLayout = (RelativeLayout) findViewById(R.id.splash_layout);
 		splashLayout.setOnTouchListener(this);
 
-		/** final MediaPlayer **/
-		logoMusic = MediaPlayer.create(SplashActivity.this, R.raw.n_man);
-		logoMusic.start();
+		if (ShumIsCool != null) {
+			isRotated = ShumIsCool.getBoolean(IS_ROTATED);
+		} else {
+			logoMusic = MediaPlayer.create(SplashActivity.this, R.raw.n_man);
+			logoMusic.start();
 
-		Thread logoTimer = new Thread() {
-			public void run() {
-				try {
+			isRotated = true;
 
-					for (int i = 0; i < 78; i++) {
-						if (isTouched) {
-							break;
-						} else {
-							sleep(100);
-						}
-					}
-
-					Intent menuIntent = new Intent("com.shumz.application.MAINACTIVITY");
-					startActivity(menuIntent);
-				}
-
-				catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				finally {
-					finish();
-				}
-			}
-		};
-		logoTimer.start();
-
+			logoTimer.start();
+		}
 	}
 
 	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		logoMusic.release();
-		// logoMusic.stop();
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putBoolean(IS_ROTATED, isRotated);
 	}
 
 	public boolean onTouch(View v, MotionEvent event) {
 		isTouched = true;
 		return false;
 	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+	}
+
 }
