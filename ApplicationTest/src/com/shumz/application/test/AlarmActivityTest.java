43499f92416b48a4bf48008217e4d3ff750ae196
Igor Shumeyko <ishumeyko@cogniance.com>
2013-12-10 05:37:05 +0200

    0041. Documentation for AlarmActivityTest.class is added in ApplicationTest project.

--- ApplicationTest/src/com/shumz/application/test/AlarmActivityTest.java
+++ ApplicationTest/src/com/shumz/application/test/AlarmActivityTest.java
@@ -4,17 +4,59 @@ import com.shumz.application.AlarmActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+/**
+ * <h6>
+ * AlarmActivityTest is a stub class, which will be implemented in future
+ * testing of Alarm Feature.</h6>
+ * <p>
+ * There is no any test written because AlarmActivity is not implemented yet.
+ * </p>
+ *  
+ * @author Igor Shumeyko
+ * @version 4.0.0
+ * @see AlarmActivity
+ * @since not implemented yet
+ */
 public class AlarmActivityTest extends
 		ActivityInstrumentationTestCase2<AlarmActivity> {
 
+	/**
+	 * <p>
+	 * Default non-argument constructor.
+	 * </p>
+	 * 
+	 * <p>
+	 * Should call superclass constructor within
+	 * <code>AlarmActivity.class</code> class under test, like:
+	 * 
+	 * <pre>
+	 *  <code>
+	 * public AlarmActivityTest() {
+	 * 	super(AlarmActivity.class);
+	 * }
+	 *  </code>
+	 * </pre>
+	 * 
+	 * </p>
+	 */
 	public AlarmActivityTest() {
 		super(AlarmActivity.class);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.test.ActivityInstrumentationTestCase2#setUp()
+	 */
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.test.ActivityInstrumentationTestCase2#tearDown()
+	 */
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
